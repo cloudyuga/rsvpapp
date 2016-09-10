d541f3ed7a6b2f91574b735e55f4759195b0a495
satyaved87 <satyaved87@users.noreply.github.com>
2016-09-10 15:29:01 +0530

    Update test_rsvpapp.py
    update

--- tests/test_rsvpapp.py
+++ tests/test_rsvpapp.py
@@ -1,22 +1,10 @@
-import mongomock
-import requests
-import rsvptest
-import unittest
+from rsvpapp import rsvptest
+import pytest
+@pytest.fixture
+def self():
+    return rsvptest.app.test_client()
+    
 
+def test_urls(self):
+    return 
 
-class RSVPAppTest(unittest.TestCase):
-    def setUp(self):
-        self.app = rsvptest.app.test_client()
-        rsvptest.client = mongomock.MongoClient()
-        rsvptest.db = rsvptest.client.mock_db_function
-
-    def test_black_box_application_response(self):
-        post_address = 'http://localhost:5000/new'
-        user_data = {'name': 'test_name', 'email': 'test_email@test_domain.com'}
-        r = requests.post(post_address, user_data)
-        self.assertEqual(r.status_code, 200)
-
-    def test_flask_app_response(self):
-        user_data = {'name': 'test_name', 'email': 'test_email@test_domain.com'}
-        status_code = self.app.post('/new', data=user_data, follow_redirects=True).status_code
-        self.assertEqual(200, status_code)
