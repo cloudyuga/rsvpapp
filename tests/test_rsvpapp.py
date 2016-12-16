2f5cbd9f78fbf8e84717d81715504b8b4f78abbe
satyaved87 <satyaved87@users.noreply.github.com>
2016-12-16 10:07:41 +0530

    Update test_rsvpapp.py

--- tests/test_rsvpapp.py
+++ tests/test_rsvpapp.py
@@ -1,10 +1,26 @@
 from rsvpapp import rsvp
+import unittest
+import mongomock
 import pytest
+from mock import Mock
+
+
+
+class RSVPApp(unittest.TestCase):
+    def setUp(self):
+        self.app = rsvp.app.test_client()
+        rsvp.client = mongomock.MongoClient()
+        rsvp.db = rsvp.client.mock_db_function
+
+    def test_Rsvp(self):
+        user_data = {'name': 'test_name', 'email': 'test_email@test_domain.com'}
+        status_code = self.app.post('/new', data=user_data, follow_redirects=True).status_code
+        self.assertEqual(200, status_code)
+
 @pytest.fixture
 def self():
     return rsvp.app.test_client()
-    
+
 
 def test_urls(self):
-    return 
-
+    return
