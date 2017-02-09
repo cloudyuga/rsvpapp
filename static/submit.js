d67af9b5089ca72ad6be2731e9268507404f73bb
satyaved87 <satyaved87@users.noreply.github.com>
2017-02-09 16:33:17 +0530

    Create submit.js

---
+++ static/submit.js
@@ -0,0 +1,19 @@
+  GNU nano 2.2.6                                   File: static/submit.js
+
+function ValidationEvent() {
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+if (name != '' && email != '') {
+if (email.match(emailReg)) {
+return true;
+} else {
+alert("Invalid Email Address...!!!");
+return false;
+}
+} else {
+alert("All fields are required.....!");
+return false;
+}
+}
