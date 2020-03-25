5b8eb092963e65fb8a521e98a3872bb6033d1771
atul7cloudyuga <53215663+atul7cloudyuga@users.noreply.github.com>
2020-03-26 02:59:55 +0530

    Update rsvp.py

--- rsvp.py
+++ rsvp.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 LINK=os.environ.get('LINK', "www.cloudyuga.guru")
 TEXT1=os.environ.get('TEXT1', "CloudYuga")
-TEXT2=os.environ.get('TEXT2', "Garage RSVP")
+TEXT2=os.environ.get('TEXT2', "Garage RSVP-atul")
 LOGO=os.environ.get('LOGO', "https://raw.githubusercontent.com/cloudyuga/rsvpapp/master/static/cloudyuga.png")
 COMPANY=os.environ.get('COMPANY', "CloudYuga Technology Pvt. Ltd.")
 
